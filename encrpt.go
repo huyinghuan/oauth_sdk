f518d1eac8b61120545503025918b1dffd973b35
huyinghuan <ec.huyinghuan@gmail.com>
2018-09-17 17:53:44 +0800

    init

---
+++ encrpt.go
@@ -0,0 +1,73 @@
+package oauth_sdk
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+func cfbEncryptBytes(keyStr string, cryptoBody []byte) (string, error) {
+	keyBytes := sha256.Sum256([]byte(keyStr))
+	return encrypt(keyBytes[:], cryptoBody)
+}
+
+func cfbEncrypt(keyStr string, cryptoText string) (string, error) {
+	keyBytes := sha256.Sum256([]byte(keyStr))
+	return encrypt(keyBytes[:], []byte(cryptoText))
+}
+
+// encrypt string to base64 crypto using AES
+func encrypt(key []byte, plaintext []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func cfbDecrypt(keyStr string, cryptoText string) (string, error) {
+	keyBytes := sha256.Sum256([]byte(keyStr))
+	return decrypt(keyBytes[:], cryptoText)
+}
+
+// decrypt from base64 to decrypted string
+func decrypt(key []byte, cryptoText string) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(cryptoText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		return "", fmt.Errorf("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+	return fmt.Sprintf("%s", ciphertext), nil
+}
---
+++ index.go
@@ -0,0 +1,136 @@
+package oauth_sdk
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"time"
+)
+
+type Scope struct {
+	Name    string   `json:"name"`
+	Type    string   `json:"type"`
+	Actions []string `json:"actions"`
+}
+
+type VerityResult struct {
+	StatusCode int
+	Scope      Scope
+}
+
+type AuthScope struct {
+	Timestamp int64 `json:"timestamp"`
+	Scope     Scope `json:"scope"`
+}
+
+type ResourceOfAcccount struct {
+	Timestamp int64  `json:"timestamp"`
+	Username  string `json:"username"`
+}
+
+func verityTimestamp(timestamp int64) bool {
+	return math.Abs(float64(timestamp-time.Now().Unix())) < 5*60
+}
+
+type SDK struct {
+	Server     string
+	PrivateKey string
+	ClientID   string
+}
+
+func (sdk *SDK) RequestResource(token string, username string) (string, error) {
+	data := map[string]interface{}{
+		"timestamp": time.Now().Unix(),
+		"token":     token,
+	}
+	dataBody, _ := json.Marshal(data)
+	encryptData, err := cfbEncrypt(sdk.PrivateKey, string(dataBody))
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest("POST", sdk.Server+"/resource/account", bytes.NewBuffer([]byte(encryptData)))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("client_id", sdk.ClientID)
+	req.Header.Set("account", username)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("Get Statsu code: %d", resp.StatusCode)
+	}
+
+	encryptResponseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	decryptResponseBody, err := cfbDecrypt(sdk.PrivateKey, string(encryptResponseBody))
+	if err != nil {
+		return "", err
+	}
+	account := ResourceOfAcccount{}
+	json.Unmarshal([]byte(decryptResponseBody), &account)
+	if !verityTimestamp(account.Timestamp) {
+		return "", fmt.Errorf("数据时间戳校验失败")
+	}
+	return account.Username, nil
+}
+
+func (sdk *SDK) Verity(path string, method string, username string) (*VerityResult, error) {
+	scope := Scope{
+		Name: path,
+		Type: method,
+	}
+
+	body, _ := json.Marshal(&scope)
+	encryptBody, err := cfbEncryptBytes(sdk.PrivateKey, body)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", sdk.Server+"/authorize", bytes.NewBuffer([]byte(encryptBody)))
+	req.Header.Set("client_id", sdk.ClientID)
+	req.Header.Set("account", username)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return &VerityResult{
+			StatusCode: resp.StatusCode,
+		}, nil
+	}
+
+	encryptResponseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	decryptResponseBody, err := cfbDecrypt(sdk.PrivateKey, string(encryptResponseBody))
+	if err != nil {
+		return nil, err
+	}
+	authScope := AuthScope{}
+	if err := json.Unmarshal([]byte(decryptResponseBody), &authScope); err != nil {
+		return nil, err
+	}
+	if !verityTimestamp(authScope.Timestamp) {
+		return nil, fmt.Errorf("校验时间戳失败")
+	}
+	return &VerityResult{
+		StatusCode: resp.StatusCode,
+		Scope:      authScope.Scope,
+	}, nil
+}
